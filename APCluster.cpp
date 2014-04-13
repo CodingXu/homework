cf319bce65f0a666d0c33be2991fc9b34b72b1e1
CodingXu <705402604@qq.com>
2014-04-13 14:33:40 +0800

    Update APCluster.cpp

--- APCluster.cpp
+++ APCluster.cpp
@@ -1,9 +1,9 @@
-//APCluster
+//AP聚类
 #include<iostream>
 #include<vector>
 using namespace std;
 
-// 
+//表示一个顶点
 struct StructPoint{
 	int id;
 	int type;
@@ -13,6 +13,26 @@ struct StructPoint{
 	}
 };
 
+//表示有起点终点权值的一条边
+struct StructEdge{
+    int srcID;
+    int desID;
+    double fValue;
+    StructEdge(){
+        srcID = 0;
+        desID = 0;
+        fValue = 0.0d;
+    }
+    StructEdge(int nIDs, int nIDd, float fV)
+	{
+		srcID = nIDs;
+		desID = nIDd;
+		fValue = fV;
+	}
+	bool operator == (const StructEdge &e) const{
+		return (srcID == e.srcID && desID == e.desID);
+	}
+};
 int main(){
 	
 	return 0;
