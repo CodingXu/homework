71fd5a46306938f0bafba6cbdabb842bb574c3a5
CodingXu <705402604@qq.com>
2014-04-14 22:35:17 +0800

    Update APCluster.cpp

--- APCluster.cpp
+++ APCluster.cpp
@@ -1,15 +1,16 @@
 //AP聚类
 #include<iostream>
 #include<vector>
+#include <list>
 using namespace std;
 
 //表示一个顶点
 struct StructPoint{
-	int id;
-	int type;
+	int nID;
+	int nType;
 	float pointX, pointY;
-	bool operator == (const StructPoint &a) const{
-		return (id == a.id);
+	bool operator == (const StructPoint &p) const{
+		return (nID == p.nID);
 	}
 };
 
@@ -23,16 +24,40 @@ struct StructEdge{
         desID = 0;
         fValue = 0.0d;
     }
-    StructEdge(int nIDs, int nIDd, float fV)
-	{
-		srcID = nIDs;
-		desID = nIDd;
-		fValue = fV;
-	}
 	bool operator == (const StructEdge &e) const{
 		return (srcID == e.srcID && desID == e.desID);
 	}
 };
+
+//计算两个数据点间的欧氏距离 
+bool calEuclidDistance(StructPoint &srcP, StructPoint &desP, double &dis){
+	dis = (srcP.x - desP.x) * (srcP.x - desP.x) + (srcP.y - desP.y) * (srcP.y - desP.y);
+	dis = - dis;
+	return true;
+}
+
+//计算所有数据点间的欧氏距离
+bool calSimilarity(list<StructPoint> &listPoint, list<StructEdge> &listSim){
+	list<StructPoint>::iterator itSrc, itDes;
+	StructEdge tempEdge;
+	for(itSrc = listPoint.begin(); itSrc != listPoint.end(); ++itSrc){
+		tempEdge.srcID = itSrc->nID;
+		itDes = itSrc;
+		for(++itDes; itDes != listPoint.end(); ++itDes){
+			tempEdge.desID = itDes->nID;
+			if(!calEuclidDistance(*itSrc, *itDes, tempEdge.fValue)){
+				cout << "计算数据点间欧氏距离出错！" << endl;
+				return false;
+			}
+			listSim.push_back(tempEdge);
+		}
+	}
+	return true;
+}
+
+//
+
+
 int main(){
 	
 	return 0;
